e154c473c19313c778a4de1fa6f3e26d0fd761a0
scavia <scaviacmhlanga@gmail.com>
2020-07-10 11:05:23 +0200

    master: add spring security configs

--- src/main/java/scaa/wardrobe/configuration/WebSecurityConfig.java
+++ src/main/java/scaa/wardrobe/configuration/WebSecurityConfig.java
@@ -1,6 +1,34 @@
 package scaa.wardrobe.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@EnableWebSecurity
+@ComponentScan("scaa.wardrobe")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder()
+                .username("scavia").password("scavia").roles("ADMIN").build());
+        return manager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security.authorizeRequests()
+                .antMatchers("/index", "/user", "/").permitAll()
+                .antMatchers("/admin").authenticated()
+                .and().formLogin().loginPage("/loginForm")
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
 }
